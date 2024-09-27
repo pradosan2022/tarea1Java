ae8de2aeae98813924390898a09928d75314ea9f
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-09-27 17:36:39 -0300

    Se crea estructura de proyecto y el primer ejercicio a modo de prueba para el uso de git con el IDE

---
+++ src/ejercicios/Ejercicio1.java
@@ -0,0 +1,15 @@
+package ejercicios;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        /*
+        * 1. Suma de dos números
+         •	Descripción: Dados los números a = 10 y b = 20, calcula la suma de ambos y muestra el resultado.
+         •	Variables: a = 10, b = 20.
+        **/
+        int a = 10;
+        int b = 20;
+
+        System.out.println("La Suma de a + b es: " + (a + b));
+    }
+}
