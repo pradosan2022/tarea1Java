98e1637e90ac6e29f5153c3e95218f1bcc2937cc
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-09-30 11:45:15 -0300

    Se finaliza el ejercicio 2.

--- src/ejercicios/Ejercicios2.java
+++ src/ejercicios/Ejercicios2.java
@@ -1,6 +1,15 @@
 package ejercicios;
 
 public class Ejercicios2 {
+    public static void main(String[] args){
+        /*
+        * Producto de dos números
+         •	Descripción: Dados los números a = 7 y b = 8, calcula el producto de ambos y muestra el resultado.
+         •	Variables: a = 7, b = 8.
+        **/
+        int a = 7;
+        int b = 8;
+        System.out.println("El Producto de a y b es: " +(a*b));
 
-
+         }
 }
