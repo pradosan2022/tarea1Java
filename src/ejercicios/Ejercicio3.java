f2fbcc32a55713dd2d86780bfacf9b186f306d99
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-10-03 13:51:32 -0300

    Se finaliza el ejercicio 3.

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -1,5 +1,18 @@
 package ejercicios;
 
 public class Ejercicio3 {
-
+    public static void main(String[] args){
+        /*
+        * Número par o impar
+•	Descripción: Dado el número a = 15, determina si es par o impar. Un número es par si es divisible por 2, y es impar si no lo es.
+•	Variables: a = 15.
+         **/
+        int a = 15;
+        int resto = a % 2;
+        if(resto==0) {
+            System.out.println("a es un número par y almacena: " + a);
+        }   else{
+                System.out.println("a es un número impar y almacena: "+a);
+            }
+    }
 }
