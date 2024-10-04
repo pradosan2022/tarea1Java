c7206389b86461f4bb2f175d3b71c37ebd661a3d
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-10-04 09:20:59 -0300

    Se inicia el ejercicio 6 para conocer el promedio de tres números.

---
+++ src/ejercicios/Ejercicio6.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        /*
+        6. Promedio de tres números
+        • Descripción: Dados los números a = 4, b = 9 y c = 13, calcula el promedio de los tres.
+        • Variables: a = 4, b = 9, c = 13.
+         */
+
+    }
+}
