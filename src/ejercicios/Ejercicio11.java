863fe5799166ff556a73f8295efa8bbc49f634ae
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-10-04 17:13:59 -0300

    Se finaliza el ejercicio 11

--- src/ejercicios/Ejercicio11.java
+++ src/ejercicios/Ejercicio11.java
@@ -7,7 +7,19 @@ public class Ejercicio11 {
         • Descripción: Genera los primeros N = 10 números de la secuencia de Fibonacci,
           donde cada número es la suma de los dos anteriores. La secuencia comienza con 0 y 1.
         • Variables: N = 10.
-
          */
+        int a = 0;
+        int b = 1;
+        int c;
+        int N = 10;
+
+        System.out.println("Los primeros "+N+" números de la secuencia de Fibonacci son: ");
+
+        for (int i=0;i<N;i++){
+            System.out.println(a);
+            c = a + b;
+            a = b;
+            b = c;
+        }
     }
 }
