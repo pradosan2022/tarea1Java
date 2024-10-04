3b8f814bec3b70cb58f88ca1038252afbf23202c
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-10-04 14:37:34 -0300

    Se inicia el ejercicio 11 para generar los primeros 10 números de la secuencia de Fibonacci

---
+++ src/ejercicios/Ejercicio11.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        /*
+        11. Fibonacci
+        • Descripción: Genera los primeros N = 10 números de la secuencia de Fibonacci,
+          donde cada número es la suma de los dos anteriores. La secuencia comienza con 0 y 1.
+        • Variables: N = 10.
+
+         */
+    }
+}
