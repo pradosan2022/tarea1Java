f2b77ddc92882054d82b0ceefeb3ad422cd0c72c
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-10-04 13:49:54 -0300

    Se inicia el ejercicio 10 para determinar el factorial de un número

---
+++ src/ejercicios/Ejercicio10.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        /*
+        10. Factorial de un número
+        • Descripción: Dado el número a = 6, calcula su factorial.
+          El factorial de un número se obtiene multiplicando todos los números desde 1 hasta el número dado.
+        • Variables: a = 6.
+         */
+
+    }
+}
