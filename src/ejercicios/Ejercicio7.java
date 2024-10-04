c19a881933d5dbff770805a3cecd3875c6edd976
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-10-04 10:07:54 -0300

    Se finaliza el ejercicio 7

--- src/ejercicios/Ejercicio7.java
+++ src/ejercicios/Ejercicio7.java
@@ -7,6 +7,10 @@ public class Ejercicio7 {
         • Descripción: Dadas la base b = 5 y la altura h = 8 de un triángulo, calcula su área usando la fórmula:
         • Variables: b = 5, h = 8.
          */
-
+        int b = 5;
+        int h = 8;
+        int area;
+        area = (b * h)/2;
+        System.out.println("Dada la base "+b+" y la altura "+h+" de un triángulo, su área es: "+area);
     }
 }
