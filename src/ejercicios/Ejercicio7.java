d216b0e1ab017e12e246245e05e321ea2c055b12
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-10-04 10:00:41 -0300

    Se inicia el ejercicio 7 para realizar el cálculo del área de un triángulo

---
+++ src/ejercicios/Ejercicio7.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        /*
+        7. Área de un triángulo
+        • Descripción: Dadas la base b = 5 y la altura h = 8 de un triángulo, calcula su área usando la fórmula:
+        • Variables: b = 5, h = 8.
+         */
+
+    }
+}
